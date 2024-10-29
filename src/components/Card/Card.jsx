347fd118042d2eb61bbadd88c0278b600e21bb17
Davi <davifssoares2002@gmail.com>
2024-10-29 17:48:28 -0300

    layout updates

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,29 @@
 import moment from 'moment';
 import './style.css';
-import { CalendarToday } from '@mui/icons-material';
+import { Article, CalendarToday, Language } from '@mui/icons-material';
 
 const Card = ({ title, distance, link, index }) => {
     console.log("link:", link)
 
     return <div className='container' key={index}>
-        <a href={`${link}`} target='_blank' rel='noopener noreferrer'>
-            <h1 className='title'>{title}</h1>
+        <div>
+            <h1 className='title'>FINEP</h1>
             <p className='text'>{distance}</p>
+            <div style={{display: "flex", flexDirection: "column"}}>
+                <div style={{display: "inline-flex", alignItems: "center", gap: ".5rem"}}>
+                    <Article className='icon' style={{width: "1rem"}}/>
+                    <a href={`${link}`} target='_blank' rel='noopener noreferrer' style={{color: "white"}}>Ver Documento</a>
+                </div>
+                <div style={{display: "inline-flex", alignItems: "center", gap: ".5rem"}}>
+                    <Language className='icon' style={{width: "1rem"}}/>
+                    <a href={`${link}`} target='_blank' rel='noopener noreferrer' style={{color: "white"}}>Ver Site</a>
+                </div>
+            </div>
             {/* <div className='dateContainer'>
                 <CalendarToday className='icon' style={{ width: "1rem" }} />
                 <p className='text'>{moment(date).format("DD/MM/YY")}</p>
             </div> */}
-        </a>
+        </div>
     </div>
 };
 
